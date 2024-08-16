3f198d0fd21db3e220d58c7a0fb8235448fa00a5
armin11 <armin.retterath@gmail.com>
2024-08-16 12:38:33 +0200

    Add admin interface fo myparty, invoice and invoiceline

--- xrechnung_light/admin.py
+++ xrechnung_light/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from xrechnung_light.models import Invoice, InvoiceLine, MyParty
 
 # Register your models here.
+admin.site.register(Invoice)
+admin.site.register(InvoiceLine)
+admin.site.register(MyParty)
